dc55e62ccb9fb8062707c7ea1244b782ea8c1f19
kevinhwang91 <kevin.hwang@live.com>
2020-10-06 11:10:34 +0800

    chore(zsh): replace zgen with zinit

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,15 +3,9 @@
 source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 
 do_install() {
-    export ZGEN_HOME="$DEFAULT_CONFIG/zsh/zgen"
-
     link_file "$src_conf/zshrc" "${ZDOTDIR:-$HOME}/.zshrc"
     mkdir -p "$DEFAULT_CONFIG/zsh" 2>/dev/null
     link_file "$src_conf/lplug" "$DEFAULT_CONFIG/zsh/lplug"
-
-    if [[ ! -d $ZGEN_HOME ]]; then
-        git clone https://github.com/tarjoilija/zgen.git "$ZGEN_HOME"
-    fi
 
     if [[ ! $SHELL =~ zsh ]]; then
         user "zsh is not your default shell, do you make it default?\n [y]es, [n]o?"
