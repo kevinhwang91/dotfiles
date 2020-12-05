1aab54e0d03d565cef7cd1c18f344b643f01809e
kevinhwang91 <kevin.hwang@live.com>
2020-12-05 21:35:38 +0800

    chore: add preview video for ranger and delete redundant of zsh

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -154,10 +154,10 @@ handle_image() {
             exit 7;;
 
         ## Video
-        # video/*)
+        video/*)
         #     # Thumbnail
-        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-        #     exit 1;;
+            ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+            exit 1;;
 
         ## PDF
         # application/pdf)
