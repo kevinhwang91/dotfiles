c48ed4b1b5388a2269f58c2969c89937bac65deb
kevinhwang91 <kevin.hwang@live.com>
2021-01-15 21:23:17 +0800

    chore: update ranger config and update bindkey for fzf

--- zsh/lplug/fzf-plugin.zsh
+++ zsh/lplug/fzf-plugin.zsh
@@ -4,7 +4,7 @@ fi
 
 FZF_DEFAULT_OPTS="--height=60% --color=fg:-1,bg:-1,hl:#7d8acc,fg+:15,bg+:#383c4a,hl+:#7d8acc \
 --color=gutter:-1,info:2,prompt:12,pointer:1,marker:3,spinner:12,header:12,border:#b3c0ce \
---bind=alt-t:top,change:top,alt-p:toggle-preview"
+--bind=alt-,:first,alt-.:last,change:first,ctrl-r:toggle-all,alt-p:toggle-preview"
 
 if (( $+commands[xsel] )); then
     FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS,ctrl-y:'execute-silent(echo {} | xsel -i -b)'"
