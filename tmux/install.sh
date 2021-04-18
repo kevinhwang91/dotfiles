8ae90a4f02cca187d065ea98036d3e8aa4de8069
kevinhwang91 <kevin.hwang@live.com>
2021-04-18 11:29:24 +0800

    feat(tmux): move tmux.conf to XDG

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,7 +3,8 @@
 source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 
 do_install() {
-    link_file "$src_conf/tmux.conf" "$HOME/.tmux.conf"
+    mkdir -p "$dst_conf" 2>/dev/null
+    link_file "$src_conf/tmux.conf" "$dst_conf/.tmux.conf"
 }
 
 install
