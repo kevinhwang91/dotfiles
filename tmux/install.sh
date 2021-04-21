e7746bc1a0002672e098ba83cb0a21b24f7f8cea
kevinhwang91 <kevin.hwang@live.com>
2021-04-21 12:26:58 +0800

    chore(tmux): abbreviate command and fix config path

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,7 +4,7 @@ source "$(cd -P "$(dirname "$0")" && pwd -P)/../base.sh"
 
 do_install() {
     mkdir -p "$dst_conf" 2>/dev/null
-    link_file "$src_conf/tmux.conf" "$dst_conf/.tmux.conf"
+    link_file "$src_conf/tmux.conf" "$dst_conf/tmux.conf"
 }
 
 install
