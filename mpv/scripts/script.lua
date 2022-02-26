e328d57f62fa5d0ecdc5cf87e2a8bbcdb40a56b6
kevinhwang91 <kevin.hwang@live.com>
2022-02-26 22:07:09 +0800

    refactor(mpv): use anonymous function prefer

--- mpv/scripts/script.lua
+++ mpv/scripts/script.lua
@@ -1,28 +1,27 @@
+---@diagnostic disable: undefined-global
 local show_osc = true
-function toggle_osc()
+
+mp.register_script_message('toggle_osc', function()
     if show_osc == true then
         mp.command('script-message osc-visibility always true')
     else
         mp.command('script-message osc-visibility auto true')
     end
     show_osc = not show_osc
-end
-
-mp.register_script_message('toggle_osc', toggle_osc)
+end)
 
 local shuffle = true
-function toggle_shuffle()
+
+mp.register_script_message('toggle_shuffle', function()
     if shuffle == true then
         mp.command('playlist-shuffle')
     else
         mp.command('playlist-unshuffle')
     end
     shuffle = not shuffle
-end
+end)
 
-mp.register_script_message('toggle_shuffle', toggle_shuffle)
-
-function toggle_loop()
+mp.register_script_message('toggle_loop', function()
     local loop_file = mp.get_property('loop-file', 'no')
     local loop_playlist = mp.get_property('loop-playlist', 'no')
     if loop_file == 'no' and loop_playlist == 'no' then
@@ -37,22 +36,13 @@ function toggle_loop()
         mp.set_property('loop-playlist', 'no')
         mp.osd_message('Loop: none')
     end
-end
+end)
 
-mp.register_script_message('toggle_loop', toggle_loop)
-
-function on_playlist_change(name, value)
-    mp.command('script-message osc-playlist 3')
-end
-
-mp.observe_property('playlist', nil, on_playlist_change)
-
-function on_pause_change(name, value)
+mp.observe_property('pause', 'bool', function(name, value)
+    local _ = name
     if value == true then
         mp.command('script-message osc-visibility always true')
     else
         mp.command('script-message osc-visibility auto true')
     end
-end
-
-mp.observe_property('pause', 'bool', on_pause_change)
+end)
